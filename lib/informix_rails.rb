131dbfa6d0ec573717b89bfd66148ba16eaee537
Michael Pope <michael@dtcorp.com.au>
2022-02-15 11:29:44 +1100

    Add convert code

--- lib/informix_rails.rb
+++ lib/informix_rails.rb
@@ -1,8 +1,33 @@
 # frozen_string_literal: true
 
 require_relative "informix_rails/version"
+require "thor"
 
 module InformixRails
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Per2Erb < Thor
+    desc "convert [file]", "Convert per to erb"
+    def convert(file)
+      File.open('ftele00a.per', 'r') do |file|
+
+        @read = false
+
+        file.readlines.each do |line|
+          if line =~ /screen/
+            @read=true
+            next
+          elsif line =~ /end/
+            @read=false
+          end
+
+          if @read
+            puts line
+          end
+
+        end
+      end
+    end
+  end
+
 end
