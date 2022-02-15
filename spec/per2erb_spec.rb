30aacc925a89f0a22959b3e4bb411b64101fb478
Michael Pope <michael@dtcorp.com.au>
2022-02-15 14:15:22 +1100

    RSpec-Refactor the tests

--- spec/per2erb_spec.rb
+++ spec/per2erb_spec.rb
@@ -1,28 +1,46 @@
 require 'spec_helper'
 require_relative '../lib/informix_rails.rb'
 
-describe "#hello" do
-  it "outputs hello world" do
-    per2erb = InformixRails::Per2Erb.new
-    expect(per2erb.hello).to eq("hello world")
+describe "Per2Erb" do
+  before do
+    @per2erb = InformixRails::Per2Erb.new
   end
-end
 
-describe "#convert" do
-  it "outputs erb" do
-    erb = "<div></div>"\
-      "\n"
-
-    expect{InformixRails::Per2Erb.start(["convert", "sample_files/ftele00a.per"])}.to output(erb).to_stdout
+  describe "#hello" do
+    it "outputs hello world" do
+      expect(@per2erb.hello).to eq("hello world")
+    end
   end
-end
 
-describe "#split_items" do
-  it "splits the line into an array of items" do
-    per2erb = InformixRails::Per2Erb.new
-    line="[l001    ][f013  ][f123                                                     ]"
-    expect(per2erb.split_items(line)[0]).to eq("l001    ")
-    expect(per2erb.split_items(line)[1]).to eq("f013  ")
-    expect(per2erb.split_items(line)[2]).to eq("f123                                                     ")
+  describe "#convert" do
+    it "outputs erb" do
+      erb = "<div></div>"\
+        "\n"
+
+      expect{InformixRails::Per2Erb.start(["convert", "sample_files/ftele00a.per"])}.to output(erb).to_stdout
+    end
+  end
+
+  describe "#split_items" do
+    it "splits the line into an array of items" do
+      line="[l001    ][f013  ][f123                                                     ]"
+      expect(@per2erb.split_items(line)[0]).to eq("l001    ")
+      expect(@per2erb.split_items(line)[1]).to eq("f013  ")
+      expect(@per2erb.split_items(line)[2]).to eq("f123                                                     ")
+    end
+  end
+
+  describe "#convert_label" do
+    describe "passing in a label" do
+      it "outputs erb" do
+
+      end
+    end
+
+    describe "passing in a text field" do
+      it "outputs nil" do
+
+      end
+    end
   end
 end
